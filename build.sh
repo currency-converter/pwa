88a89c3917bbd88b5d8ae75cec7b977eea7907f7
zhi.zhong <zhi.zhong@qunar.com>
2018-01-29 11:33:43 +0800

    build: Add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PATH=/usr/local/n/versions/node/6.2.1/bin:$PATH
+
+if [ ! -n "$1" ] ;then
+  echo 参数错误
+  echo usage: build.sh target_name
+else
+  yarn
+  echo target_name: $1
+  yarn build:$1
+  # mv prd/templates templates
+fi
